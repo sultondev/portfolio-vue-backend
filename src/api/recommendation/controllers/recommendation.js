6d9cfbaee2609b066884525dd06c90780256d0f4
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-11-22 23:50:58 +0500

    bismillah

---
+++ src/api/recommendation/controllers/recommendation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recommendation controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::recommendation.recommendation');
---
+++ src/api/recommendation/routes/recommendation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recommendation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::recommendation.recommendation');
