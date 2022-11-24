fc5339ddbd481df4b79de5ae86bd3d7311a4dba0
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-11-25 01:02:48 +0500

    change portfolio api findOne field to work with slug

--- src/api/portfolio/controllers/portfolio.js
+++ src/api/portfolio/controllers/portfolio.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::portfolio.portfolio');
+module.exports = createCoreController('api::portfolio.portfolio', ({ strapi }) => ({
+
+    // Method 3: Replacing a core action
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const entity = await strapi.db.query('api::portfolio.portfolio').findOne({
+            where: { slug: id }
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
